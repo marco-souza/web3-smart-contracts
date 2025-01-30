31b241c4623124a897af1e02e67f3a613689b41f
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-30 16:38:36 -0300

    fix: await for deployment

--- apps/contracts/test/HelloWorld.test.ts
+++ apps/contracts/test/HelloWorld.test.ts
@@ -5,6 +5,7 @@ describe("HelloWorld", () => {
   it("Should return the right greeting", async () => {
     const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
     const helloWorld = await HelloWorld.deploy();
+    await helloWorld.waitForDeployment()
 
     expect(await helloWorld.hello()).to.equal("Hello, World!");
   })
