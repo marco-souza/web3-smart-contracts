f11647c07f7415d65dce270de19a7baead4c6d02
Marco Antônio <ma.souza.junior@gmail.com>
2025-02-04 20:16:30 -0300

    fea: set minimum page to test contracts

---
+++ apps/contracts/page/server.ts
@@ -0,0 +1,36 @@
+async function main() {
+  await Bun.build({
+    entrypoints: ["./index.ts"],
+    outdir: "./dist",
+  })
+
+  return Bun.serve({
+    port: 6969,
+    static: {
+      // serve a file by buffering it in memory
+      "/": new Response(await Bun.file("./index.html").bytes(), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      }),
+
+      "/dist/index.js": new Response(await Bun.file("./dist/index.js").bytes(), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      }),
+
+      // serve JSON
+      "/version.json": Response.json({ version: "1.0.0" }),
+    },
+
+    fetch(req) {
+      return new Response("404!");
+    },
+  });
+}
+
+main()
+  .then((s) => console.log(`Server started!
+    http://${s.hostname}:${s.port}`))
+  .catch(console.error);
