48ac51eb26f7585dd3f60bab770d458756024a04
Marco Antônio <ma.souza.junior@gmail.com>
2025-02-04 16:02:53 -0300

    fea: add counter contract

---
+++ apps/contracts/contracts/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+contract Counter {
+  uint counter;
+
+  function count() public returns (uint) {
+    counter++;
+    console.log("Counter is now", counter);
+    return counter;
+  }
+}
