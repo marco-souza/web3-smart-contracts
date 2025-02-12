3c5bcf643dc17cf455bb5f9ca7c1aa0a0c8965b0
Marco Antônio <ma.souza.junior@gmail.com>
2025-02-11 21:13:21 -0300

    fea: test random hero with random stats

--- apps/contracts/test/Hero.test.ts
+++ apps/contracts/test/Hero.test.ts
@@ -43,3 +43,59 @@ describe("Hero", () => {
     expect(typeof randomNum).to.equal("bigint");
   })
 })
+
+
+describe("TestHero", () => {
+  async function createHero() {
+    const Hero = await hre.ethers.getContractFactory("TestHero");
+    const hero = await Hero.deploy();
+    await hero.waitForDeployment();
+
+    return hero;
+  }
+
+  it("check if empty array", async () => {
+    const hero = await createHero();
+    expect(await hero.getHeroes()).to.lengthOf(0);
+  })
+
+  it("check the class of a created hero", async () => {
+    const hero = await createHero();
+
+    await hero.setRandom(69);
+    await hero.createHero(2, { value: ethers.parseEther("0.05") });
+
+    await hero.setRandom(420);
+    await hero.createHero(0, { value: ethers.parseEther("0.05") });
+
+    const [h1, h2] = await hero.getHeroes();
+
+    // [ C, S, H, D, I, M ]
+    expect(await hero.getClass(h1)).to.equal(2);
+    // [ S, H, D, I, M ], p: 4, v: 16
+    expect(await hero.getMagic(h1)).to.equal(16);
+    // [ S, H, D, I ],    p: 1, v: 2
+    expect(await hero.getHealth(h1)).to.equal(2);
+    // [ S, I, D ],       p: 0, v: 6
+    expect(await hero.getStrength(h1)).to.equal(6);
+    // [ D, I ],          p: 1, v: 10
+    expect(await hero.getIntellect(h1)).to.equal(10);
+    // [ D ],             p: 0, v: 14
+    expect(await hero.getDexterity(h1)).to.equal(14);
+
+    // [ C, S, H, D, I, M ]
+    expect(await hero.getClass(h2)).to.equal(0);
+    // [ S, H, D, I, M ], p: 0, v: 7
+    expect(await hero.getStrength(h2)).to.equal(7);
+    // [ M, H, D, I ],    p: 0, v: 13
+    expect(await hero.getMagic(h2)).to.equal(13);
+    // [ I, H, D ],       p: 0, v: 5
+    expect(await hero.getIntellect(h2)).to.equal(5);
+    // [ D, H ],          p: 0, v: 1
+    expect(await hero.getDexterity(h2)).to.equal(1);
+    // [ H ],             p: 0, v: 1
+    expect(await hero.getHealth(h2)).to.equal(1);
+
+  })
+})
+
